c0c20f7e910ed4ed62b4faa378d8debecda61a2a
JORGE GARCÍA LEÓN <usr2691@salesianos-lacuesta.net>
2021-05-20 11:57:28 +0100

    Constructor sin funcionar

---
+++ prueba/src/contador.java
@@ -0,0 +1,5 @@
+public class contador {
+    public static int valorInicial = 0;
+    public static int valorLimite = 10;
+    public static int incremento = 1;
+}
---
+++ prueba/src/testContador.java
@@ -0,0 +1,10 @@
+import  org.junit.*;
+
+public class testContador {
+    
+    @Test
+    public void comprobarConstructor(){
+        contador c = new contador();
+    }
+
+}
