7c70a5729d284e856dc38e374ff1c4182dd9886f
JORGE GARCÍA LEÓN <usr2691@salesianos-lacuesta.net>
2021-05-20 11:59:09 +0100

    Constructor arreglado

--- prueba/src/contador.java
+++ prueba/src/contador.java
@@ -2,4 +2,14 @@ public class contador {
     public static int valorInicial = 0;
     public static int valorLimite = 10;
     public static int incremento = 1;
+
+    public contador(){
+
+    }
+
+    public contador(int valorInicial, int valorLimite, int incremento){
+        this.valorInicial = valorInicial;
+        this.valorLimite = valorLimite;
+        this.incremento = incremento;
+    }
 }
